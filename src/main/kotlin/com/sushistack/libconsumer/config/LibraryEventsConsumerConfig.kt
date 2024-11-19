273e814a6ae0efaded75c3f9778c9c70df09a9a9
sushistack <kjm71712@naver.com>
2024-11-19 15:25:57 +0900

    set concrruency

--- src/main/kotlin/com/sushistack/libconsumer/config/LibraryEventsConsumerConfig.kt
+++ src/main/kotlin/com/sushistack/libconsumer/config/LibraryEventsConsumerConfig.kt
@@ -24,6 +24,12 @@ class LibraryEventsConsumerConfig {
         configurer.configure(factory, kafkaConsumerFactory)
         // manually ack processing by LibraryEventsConsumerManualOffset
         // factory.containerProperties.ackMode = ContainerProperties.AckMode.MANUAL
+
+
+        factory.setConcurrency(3)
+        // 2024-11-19T15:24:36.765+09:00  INFO 78698 --- [library-events-consumer] [ntainer#0-0-C-1] o.s.k.l.KafkaMessageListenerContainer    : library-events-listener-group: partitions assigned: [library-events-0]
+        // 2024-11-19T15:24:36.770+09:00  INFO 78698 --- [library-events-consumer] [ntainer#0-1-C-1] o.s.k.l.KafkaMessageListenerContainer    : library-events-listener-group: partitions assigned: [library-events-1]
+        // 2024-11-19T15:24:36.774+09:00  INFO 78698 --- [library-events-consumer] [ntainer#0-2-C-1] o.s.k.l.KafkaMessageListenerContainer    : library-events-listener-group: partitions assigned: [library-events-2]
         return factory
     }
 
