0bf4896378ba95e3f5a8cd4bfc3965ea9fd2bba3
sushistack <kjm71712@naver.com>
2024-11-19 15:21:13 +0900

    add manually ack processing

--- src/main/kotlin/com/sushistack/libconsumer/config/LibraryEventsConsumerConfig.kt
+++ src/main/kotlin/com/sushistack/libconsumer/config/LibraryEventsConsumerConfig.kt
@@ -1,10 +1,30 @@
 package com.sushistack.libconsumer.config
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
+import org.springframework.boot.autoconfigure.kafka.ConcurrentKafkaListenerContainerFactoryConfigurer
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.kafka.annotation.EnableKafka
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory
+import org.springframework.kafka.core.ConsumerFactory
+import org.springframework.kafka.listener.ContainerProperties
+
 
 @EnableKafka
 @Configuration
 class LibraryEventsConsumerConfig {
+
+    @Bean
+    @ConditionalOnMissingBean(name = ["kafkaListenerContainerFactory"])
+    fun kafkaListenerContainerFactory(
+        configurer: ConcurrentKafkaListenerContainerFactoryConfigurer,
+        kafkaConsumerFactory: ConsumerFactory<Any, Any>
+    ): ConcurrentKafkaListenerContainerFactory<*, *> {
+        val factory = ConcurrentKafkaListenerContainerFactory<Any, Any>()
+        configurer.configure(factory, kafkaConsumerFactory)
+        // manually ack processing by LibraryEventsConsumerManualOffset
+        factory.containerProperties.ackMode = ContainerProperties.AckMode.MANUAL
+        return factory
+    }
 
 }
---
+++ src/main/kotlin/com/sushistack/libconsumer/consumer/LibraryEventsConsumerManualOffset.kt
@@ -0,0 +1,20 @@
+package com.sushistack.libconsumer.consumer
+
+import io.github.oshai.kotlinlogging.KotlinLogging
+import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.springframework.kafka.annotation.KafkaListener
+import org.springframework.kafka.listener.AcknowledgingMessageListener
+import org.springframework.kafka.support.Acknowledgment
+import org.springframework.stereotype.Component
+
+@Component
+class LibraryEventsConsumerManualOffset: AcknowledgingMessageListener<Long, String> {
+
+    private val log = KotlinLogging.logger {}
+
+    @KafkaListener(topics = ["library-events"], groupId = "library-events-listener-group")
+    override fun onMessage(data: ConsumerRecord<Long, String>, acknowledgment: Acknowledgment?) {
+        log.info { "ConsumerRecord : $data" }
+        acknowledgment?.acknowledge()
+    }
+}
